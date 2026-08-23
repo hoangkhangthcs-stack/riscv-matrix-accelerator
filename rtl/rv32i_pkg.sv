218fe814e3f0ff562e588f117a63f319a9a18b4c
Ten cua ban <email@example.com>
2026-08-23 11:09:00 +0700

    feat: implement immediate generator

--- rtl/rv32i_pkg.sv
+++ rtl/rv32i_pkg.sv
@@ -3,4 +3,8 @@ package rv32i_pkg;
         ALU_ADD, ALU_SUB, ALU_SLL, ALU_SLT, ALU_SLTU, 
         ALU_XOR, ALU_SRL, ALU_SRA, ALU_OR, ALU_AND
     }alu_op_e;
+
+    typedef enum logic [2:0]{
+        IMM_I, IMM_S, IMM_B, IMM_U, IMM_J
+    }imm_type_e;
 endpackage
